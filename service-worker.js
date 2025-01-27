0bc8800c20790502c30cce35ef0240e5834e14f5
Stalin Thangaraj <stalin.jt@gmail.com>
2025-01-27 17:25:58 +0530

    fix

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'pwa-bookmark-io-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/bookmark.html',
-  '/styles.css',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
+  '/pwa-bookmark.github.io/',
+  '/pwa-bookmark.github.io/index.html',
+  '/pwa-bookmark.github.io/bookmark.html',
+  '/pwa-bookmark.github.io/styles.css',
+  '/pwa-bookmark.github.io/manifest.json',
+  '/pwa-bookmark.github.io/icons/icon-192x192.png',
+  '/pwa-bookmark.github.io/icons/icon-512x512.png',
 ];
 
 self.addEventListener('install', (event) => {
